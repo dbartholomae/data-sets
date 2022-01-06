9c97b130ca2813a6e603d767f81e2e5cfcf5798b
Daniel Bartholomae <daniel@bartholomae.name>
2022-01-06 22:08:20 +0100

    chore: do not ensure coverage
    For datasets, that doesn't make that much sense

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,6 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*"],
-  coverageDirectory: "coverage",
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   testRegex: ".*test\\.(t|j)sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
